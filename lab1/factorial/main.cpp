dfdaf672591c92b58bd0ce8ed5e2232aa7dd1b43
gtluszcz <gtluszcz@gmail.com>
2017-03-14 10:19:26 +0100

    Commit done

--- lab1/factorial/main.cpp
+++ lab1/factorial/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "Factorial.h"
 
 int main() {
---
+++ lab2/polybius/Polybius.h
@@ -0,0 +1,17 @@
+//
+// Created by Grzegorz on 07.03.2017.
+//
+
+#ifndef JIMP_EXERCISES_POLYBIUS_H
+#define JIMP_EXERCISES_POLYBIUS_H
+#include <string>
+#include <string>
+#include <map>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+std::string PolybiusCrypt(std::string message);
+std::string PolybiusDecrypt(std::string crypted);
+
+#endif //JIMP_EXERCISES_POLYBIUS_H
