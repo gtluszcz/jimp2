94f98a2ddc75e6cc15799115736771e0fe5e6446
gtluszcz <gtluszcz@gmail.com>
2017-03-16 13:02:10 +0100

    Start of tinyurl

---
+++ lab3/tinyurl/TinyUrl.h
@@ -0,0 +1,16 @@
+//
+// Created by Grzegorz on 16.03.2017.
+//
+
+#ifndef JIMP_EXERCISES_TINYURL_H
+#define JIMP_EXERCISES_TINYURL_H
+#include <utility>
+#include <string>
+#include <array>
+#include <memory>
+std::unique_ptr<TinyUrlCodec> Init();
+void NextHash(std::arrray<char, 6> *state);
+std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
+std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+
+#endif //JIMP_EXERCISES_TINYURL_H
---
+++ lab3/tinyurl/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Grzegorz on 16.03.2017.
+//
+
+#include "TinyUrl.h"
+#include <utility>
+#include <string>
+#include <array>
+#include <memory>
+using namespace std;
+
+int main(){
+    cout<<"Hello"<<endl;
+}
