aa862dd4a01a0360a57445ea51622359e6d067a2
gtluszcz <gtluszcz@gmail.com>
2017-03-16 13:25:04 +0100

    Start of tinyurl

--- lab3/tinyurl/TinyUrl.cpp
+++ lab3/tinyurl/TinyUrl.cpp
@@ -7,17 +7,23 @@
 #include <string>
 #include <array>
 #include <memory>
-using namespace std;
 
-std::unique_ptr<TinyUrlCodec> Init(){
 
-}
-void NextHash(std::arrray<char, 6> *state){
 
-}
-std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec){
+namespace tinyurl {
+    std::unique_ptr<TinyUrlCodec> Init() {
 
-}
-std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash){
+    }
 
+    void NextHash(std::arrray<char, 6> *state) {
+
+    }
+
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec) {
+
+    }
+
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash) {
+
+    }
 }
--- lab3/tinyurl/TinyUrl.h
+++ lab3/tinyurl/TinyUrl.h
@@ -8,9 +8,15 @@
 #include <string>
 #include <array>
 #include <memory>
-std::unique_ptr<TinyUrlCodec> Init();
-void NextHash(std::arrray<char, 6> *state);
-std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
-std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+namespace tinyurl {
+
+    std::unique_ptr<TinyUrlCodec> Init();
+
+    void NextHash(std::arrray<char, 6> *state);
+
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
+
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+}
 
 #endif //JIMP_EXERCISES_TINYURL_H
