c9bdcbf7fd8d5bc22a563a3f6b71174c9939bd37
gtluszcz <gtluszcz@gmail.com>
2017-04-04 09:29:24 +0200

    lab5 textpool ready

--- lab5/textpool/TextPool.h
+++ lab5/textpool/TextPool.h
@@ -8,16 +8,39 @@
 #include <string>
 #include <experimental/string_view>
 #include <set>
+#include <vector>
+using std::experimental::string_view;
 
 namespace pool {
     class TextPool {
     protected:
-
+        std::set<const string_view> tekst;
 
     public:
-        Rule of Five (z usuniętą możliwością kopiowania)
-        domyślny konstruktor
-        konstruktor z listą inicjalizacyjną
+        //Rule of Five (z usuniętą możliwością kopiowania)
+
+        //1. konstruktor kopiujący
+        //TextPool(const TextPool &xxx);
+
+        //2. konstruktor przenoszący
+        TextPool(TextPool &&xxx);
+
+        //3. operator przypisania kopiujący
+        //TextPool &operator=(const TextPool &xxx);
+
+        //4. operator przypisania przenoszący
+        TextPool &operator=(TextPool &&xxx);
+
+        //5. Destruktor
+        ~TextPool();
+
+
+        //domyślny konstruktor
+        TextPool();
+
+        //konstruktor z listą inicjalizacyjną
+        TextPool(const std::initializer_list<const std::string> &elements);
+
         std::experimental::string_view Intern(const std::string &str);
         size_t StoredStringCount() const;
 
--- lab5/textpool/main.cpp
+++ lab5/textpool/main.cpp
@@ -1,4 +1,29 @@
 //
 // Created by Grzegorz on 28.03.2017.
 //
+#include <iostream>
+#include "TextPool.h"
 
+using namespace pool;
+using namespace std;
+
+int main() {
+    //Inicjalizacja wstępna puli za pomocą listy inicjalizacyjnej
+    TextPool pool {"abc", "efg", "hij", "klmn", "oprst"};
+    TextPool new1(move(pool));
+    //TextPool new2;
+    //new1=move(new2=move(pool));
+    cout << new1.StoredStringCount() << endl;
+
+    //wstawienie napisu do puli
+    auto s1 = new1.Intern("lel");
+    cout << new1.StoredStringCount() << endl;
+
+    //wstawienie kolejnego napisu do puli (w obu przypadkach nie
+    //powinien się zmienić rozmiar puli)
+    auto s2 = new1.Intern("lel");
+
+    cout << (s1 == s2 ? "True" : "False") << endl; //uchwyty są tymi samymi napisami co do wartości
+    cout << new1.StoredStringCount() << endl; // w puli jest wciąż 5 napisów
+    cout << (s1.begin() == s2.begin() ? "True" : "False") << endl; //na dodatek uchywyty s1 i s2 pokazują dokładnie na ten sam napis w puli (wskaźniki są identyczne)
+}
