853257c46704f1b91b9d224ff543fb6a41dfc4a3
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-04-14 17:36:47 +0200

    Fix lab7 figures

---
+++ lab7/figures/Figures.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Dariusz Czajkowski on 14/04/2017.
+//
+
+#include "Figures.h"
+
+Circle::Circle(double _x, double _y, double _r) {
+    x = _x;
+    y = _y;
+    r = _r;
+}
+
+double Circle::surface() {
+    return PI * pow(r, 2);
+}
+
+Sphere::Sphere(double _x, double _y, double _z, double _r) : Circle(_x, _y, _r) {
+    z = _z;
+}
+
+double Sphere::surface() {
+    return 4 * PI * pow(r, 2);
+}
---
+++ lab7/figures/Figures.h
@@ -0,0 +1,33 @@
+//
+// Created by Dariusz Czajkowski on 14/04/2017.
+//
+
+#ifndef JIMP_EXERCISES_FIGURES_H
+#define JIMP_EXERCISES_FIGURES_H
+
+#include <math.h>
+
+const double PI = 3.14159265359;
+
+class Circle {
+protected:
+    double x;
+    double y;
+    double r;
+
+public:
+    Circle(double _x, double _y, double _r);
+    double surface();
+};
+
+class Sphere : public Circle {
+protected:
+    double z;
+
+public:
+    Sphere(double _x, double _y, double _z, double _r);
+    double surface();
+};
+
+
+#endif //JIMP_EXERCISES_FIGURES_H
