66790f958c0a590ab7537a1cd52b3410641b3680
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-04-14 18:49:26 +0200

    Add comments

--- lab7/point3d/main.cpp
+++ lab7/point3d/main.cpp
@@ -9,14 +9,31 @@ using ::std::cout;
 using ::std::endl;
 
 int main() {
-    Point3d point3d(10, 12, 2);
-    Point point2d(3, 5);
+    /**
+     * Order of constructors and destructors is:
+     * 1. 'Point' class - parameter constructor - point3d object
+     * 2. 'Point3d' class - parameter constructor - point3d object
+     * 3. 'Point' class - parameter constructor - point2d object
+     * 4. 'Point' class - destructor - point3d object
+     * 5. 'Point3d' class - destructor - point3d object
+     * 6. 'Point' class - destructor - point2d object
+     */
+    Point3d point3d(0, 0, 1);
+    Point point2d(1, 1);
 
+    /**
+     * The distance has been calculated as if the third dimension didn't exist.
+     * The 'Point' type-hint allowed the 'Point3d' object to be passed in as
+     * it extends the required 'Point' class.
+     */
     cout << point2d.Distance(point3d) << endl;
 
 
+    /**
+     * The cout doesn't work as the operator<< has not been defined anywhere.
+     */
     Point3d newPoint3d(1, 2, 3);
-    cout << newPoint3d << endl;
+    // cout << newPoint3d << endl;
 
     return 0;
 }
