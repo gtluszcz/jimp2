462f7b583a7b162721d710a70ff80544580b5857
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-05-09 09:54:29 +0200

    Includes

--- lab9/juwenalia/products/Vodka.h
+++ lab9/juwenalia/products/Vodka.h
@@ -1,6 +1,7 @@
 #ifndef JIMP_EXERCISES_VODKA_H
 #define JIMP_EXERCISES_VODKA_H
 
+#include "Product.h"
 
 class Vodka : public Product {
 
--- lab9/juwenalia/products/Water.h
+++ lab9/juwenalia/products/Water.h
@@ -1,6 +1,7 @@
 #ifndef JIMP_EXERCISES_WATER_H
 #define JIMP_EXERCISES_WATER_H
 
+#include "Product.h"
 
 class Water : public Product {
 
