1f4d5969e3bf19cf4b7e7cd661784d0d31290ccd
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-05-26 00:00:57 +0200

    Better formatting

--- lab10/academiascheduler/Scheduler.cpp
+++ lab10/academiascheduler/Scheduler.cpp
@@ -1,16 +1,20 @@
 #include "Scheduler.h"
+#include <utility>
+#include <algorithm>
 
 using academia::SchedulingItem;
 using academia::Schedule;
 using academia::Scheduler;
+using std::pair;
+using std::count;
+using std::find;
 
 /**
  * SchedulingItem
  */
 
-SchedulingItem::SchedulingItem(int course_id, int teacher_id, int room_id, int time_slot, int year) : course_id(course_id), teacher_id(teacher_id), room_id(room_id), time_slot(time_slot), year(year) {
-    //
-}
+SchedulingItem::SchedulingItem(int course_id, int teacher_id, int room_id, int time_slot, int year)
+    : course_id(course_id), teacher_id(teacher_id), room_id(room_id), time_slot(time_slot), year(year) {}
 
 /**
  * Schedule
@@ -49,17 +53,17 @@ Schedule Schedule::OfYear(int year) const {
 vector<int> Schedule::AvailableTimeSlots(int n_time_slots) const {
     vector<int> free_slots;
     vector<int> bad_slots;
-    for (auto & el: schedule_items){
+
+    for (auto & el : schedule_items){
         bad_slots.push_back(el->TimeSlot());
     }
-    for (int i = 1; i <= n_time_slots; ++i) {
-        if (std::find(bad_slots.begin(),bad_slots.end(),i) != bad_slots.end()){
 
-        }
-        else{
-        free_slots.push_back(i);
+    for (int i = 1; i <= n_time_slots; ++i) {
+        if (find(bad_slots.begin(), bad_slots.end(), i) == bad_slots.end()){
+            free_slots.push_back(i);
         }
     }
+
     return free_slots;
 }
 
@@ -70,80 +74,76 @@ void Schedule::InsertScheduleItem(const SchedulingItem &item) {
 size_t Schedule::Size() const {
     return schedule_items.size();
 }
+
 const SchedulingItem & Schedule::operator[](int id) const {
     return *(schedule_items[id]);
 }
-
 
 /**
  * Scheduler
  */
 
-Schedule Scheduler::PrepareNewSchedule(
+Schedule academia::GreedyScheduler::PrepareNewSchedule(
     const vector<int> &rooms,
     const map<int, vector<int>> &teacher_courses_assignment,
     const map<int, set<int>> &courses_of_year,
     int n_time_slots
 ) {
-    //
-}
-
-Schedule academia::GreedyScheduler::PrepareNewSchedule(const std::vector<int> &rooms,
-                                                                 const std::map<int, std::vector<int>> &teacher_courses_assignment,
-                                                                 const std::map<int, std::set<int>> &courses_of_year,
-                                                                 int n_time_slots) {
-
-    std::vector<SchedulingItem> scheduling;
-    std::vector<std::pair<int, int>> rooms_time_slots_pairs;
+    vector<SchedulingItem> scheduling_items;
     int number_of_courses;
+
     for (auto year_courses : courses_of_year) {
         for (auto course : year_courses.second) {
             number_of_courses = 0;
+
             for (auto teacher_courses : teacher_courses_assignment) {
-                number_of_courses += std::count(teacher_courses.second.begin(), teacher_courses.second.end(), course);
+                number_of_courses += count(teacher_courses.second.begin(), teacher_courses.second.end(), course);
             }
-            if ( number_of_courses > n_time_slots)
+
+            if (number_of_courses > n_time_slots) {
                 throw NoViableSolutionFound();
-        }
-    }
-
-    for(auto teacher_course : teacher_courses_assignment) {
-        for(auto course : teacher_course.second) {
-            for(auto year_course : courses_of_year) {
-                if(year_course.second.find(course) != year_course.second.end())
-                    scheduling.push_back(SchedulingItem(course, teacher_course.first, 0, 0, year_course.first));
             }
         }
     }
 
-    if(scheduling.size() > rooms.size() * n_time_slots)
-        throw NoViableSolutionFound{};
+    for (auto teacher_course : teacher_courses_assignment) {
+        for (auto course : teacher_course.second) {
+            for (auto year_course : courses_of_year) {
+                if (year_course.second.find(course) != year_course.second.end()) {
+                    scheduling_items.push_back(SchedulingItem(course, teacher_course.first, 0, 0, year_course.first));
+                }
+            }
+        }
+    }
 
-    int i =0;
-    for(auto room : rooms) {
+    if (scheduling_items.size() > rooms.size() * n_time_slots) {
+        throw NoViableSolutionFound();
+    }
+
+    int i = 0;
+    for (auto room : rooms) {
         for (int time_slot = 1; time_slot <= n_time_slots; time_slot++){
-            if(i<scheduling.size()) {
-                scheduling[i].time_slot = time_slot;
-                scheduling[i].room_id = room;
+            if (i < scheduling_items.size()) {
+                scheduling_items[i].time_slot = time_slot;
+                scheduling_items[i].room_id = room;
             }
             i++;
         }
     }
-    
-    Schedule newone;
-    for(auto item: scheduling){
-        newone.InsertScheduleItem(SchedulingItem{item.course_id, item.teacher_id, item.room_id, item.time_slot, item.year});
+
+    Schedule tmp;
+    for (auto item : scheduling_items) {
+        tmp.InsertScheduleItem(SchedulingItem {item.course_id, item.teacher_id, item.room_id, item.time_slot, item.year});
     }
-    return newone;
+    return tmp;
 }
 
 /**
  * Getters
  */
 
-const int SchedulingItem::CourseId() const  { return course_id; }
-const int SchedulingItem::TeacherId() const  { return teacher_id; }
+const int SchedulingItem::CourseId() const { return course_id; }
+const int SchedulingItem::TeacherId() const { return teacher_id; }
 const int SchedulingItem::RoomId() const { return room_id; }
-const int SchedulingItem::TimeSlot() const  { return time_slot; }
-const int SchedulingItem::Year() const  { return year; }
-
+const int SchedulingItem::TimeSlot() const { return time_slot; }
+const int SchedulingItem::Year() const { return year; }
--- lab10/academiascheduler/Scheduler.h
+++ lab10/academiascheduler/Scheduler.h
@@ -13,31 +13,28 @@ namespace academia {
     class SchedulingItem {
     public:
         SchedulingItem(int course_id, int teacher_id, int room_id, int time_slot, int year);
-        const int CourseId() const ;
-        const int TeacherId() const ;
-        const int RoomId() const ;
-        const int TimeSlot() const ;
+        const int CourseId() const;
+        const int TeacherId() const;
+        const int RoomId() const;
+        const int TimeSlot() const;
         const int Year() const;
 
-
-        int course_id; // identyfikator kursu
-        int teacher_id; // identyfikator nauczyciela akademickiego prowadzącego te zajęcia o tej godzinie
-        int room_id; // identyfikator pomieszczenia w którym odbywają się zajęcia
-        int time_slot; // okno czasowe, w którym odbywają się zajęcia, dla uproszczenia cały tydzień jest podzielony na N okien czasowych nie zachodzących na siebie (np. dla 20: 1 ⇒ pon 8:00-10:00, 2 ⇒ pon 10:00-12:00, … 20 ⇒ pią 14:00-16:00). Zajęcia wymagają zawsze pojedynczego okna czasowego i zawsze są z nim wyrównane tzn. nie mogą zajmować połowę 12ego i połowę 13ego
-        int year; // rok studiów na którym obowiązuje przedmiot
+        int course_id;
+        int teacher_id;
+        int room_id;
+        int time_slot;
+        int year;
     };
 
     class Schedule {
     public:
-        Schedule OfTeacher(int teacher_id) const; // wylicza fragment harmonogramu związany z danym nauczycielem akademickim (może się przydać copy_if…)
-        Schedule OfRoom(int room_id) const; // j.w. tylko związany z danym pomieszczeniem
-        Schedule OfYear(int year) const; // j.w. dla danego roku studiów
-        vector<int> AvailableTimeSlots(int n_time_slots) const; // wylicza wektor jeszcze nie zajętych okien czasowych, gdzie n_time_slots jest maksymalną wartością okna czasowego
-        void InsertScheduleItem(const SchedulingItem &item); // wstawia nowy element planu
-        size_t Size() const; // zwaraca rozmiar planu
+        Schedule OfTeacher(int teacher_id) const;
+        Schedule OfRoom(int room_id) const;
+        Schedule OfYear(int year) const;
+        vector<int> AvailableTimeSlots(int n_time_slots) const;
+        void InsertScheduleItem(const SchedulingItem &item);
+        size_t Size() const;
         const SchedulingItem & operator[](int id) const;
-
-
 
         vector<const SchedulingItem *> schedule_items;
     };
@@ -52,19 +49,17 @@ namespace academia {
         );
     };
 
-    // Wyjątek wyrzucany, gdy nie udaje się stworzyć planu
     class NoViableSolutionFound {};
 
-    // Wersja zachłanna implentacji algorytmu układacza planu
-    class GreedyScheduler: public Scheduler {
+    class GreedyScheduler : public Scheduler {
     public:
         Schedule PrepareNewSchedule(
-                const vector<int> &rooms,
-                const map<int, vector<int>> &teacher_courses_assignment,
-                const map<int, set<int>> &courses_of_year,
-                int n_time_slots
+            const vector<int> &rooms,
+            const map<int, vector<int>> &teacher_courses_assignment,
+            const map<int, set<int>> &courses_of_year,
+            int n_time_slots
         );
     };
 }
 
-#endif //JIMP_EXERCISES_SCHEDULER_H
+#endif
