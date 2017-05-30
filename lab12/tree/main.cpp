608f158b5bcc2e9488769279dfdbdb6f6a9695a1
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-05-30 10:08:53 +0200

    No so many spaces

--- lab12/tree/main.cpp
+++ lab12/tree/main.cpp
@@ -5,10 +5,10 @@ using tree::Tree;
 
 int main() {
     vector<Leaf> leaves = {
-            Leaf {10},
-            Leaf {10.1},
-            Leaf {"9"},
-            Leaf {"9.1"}
+        Leaf {10},
+        Leaf {10.1},
+        Leaf {"9"},
+        Leaf {"9.1"}
     };
 
     Tree tree(leaves);
