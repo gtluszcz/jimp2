2cce9b96d292d57398aa0e48fc380f1ad2ec8687
gtluszcz <moniuszkO2005>
2017-05-30 09:40:24 +0200

    Merge branch 'master' of https://github.com/gtluszcz/jimp2
    # Conflicts:
    #	lab10/algotest/Algo.h

---
+++ lab11/factorymethod/Factorymethod.h
@@ -0,0 +1,17 @@
+//
+// Created by tlusgrze on 23.05.17.
+//
+
+#ifndef JIMP_EXERCISES_FACTORY_H
+#define JIMP_EXERCISES_FACTORY_H
+
+//template<class T>
+//class Factory{
+//    public:
+//        void factorymethod();
+//};
+
+template <class one>
+one Sum(one left,one right);
+
+#endif //JIMP_EXERCISES_FACTORY_H
---
+++ lab11/factorymethod/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by tlusgrze on 23.05.17.
+//
+#include "Factorymethod.h"
+#include <iostream>
+using namespace std;
+
+//template <class one>
+//one Sum(one left,one right){
+//    return left + right;
+//};
+
+int main(){
+//    Factory<int> obiekt;
+//     obiekt.factorymethod<int>();
+    cout<<Sum(3.1,4.2);
+
+}
