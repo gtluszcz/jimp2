6854d961461ac4ea9947efc488b9f1fc556c4045
gtluszcz <gtluszcz@wp.pl>
2017-06-11 17:07:18 +0200

    teacherhash

---
+++ lab12/teacherhash/TeacherHash.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Grzegorz on 11.06.2017.
+//
+
+#include "TeacherHash.h"
+using namespace academia;
+
+
+//TeacherId
+TeacherId::TeacherId() {
+    value_=0;
+}
+TeacherId::TeacherId(int value) {
+    value_=value;
+}
+TeacherId::operator int() const {
+    return this->value_;
+}
+
+
+
+//Teacher
+Teacher::Teacher(TeacherId id, std::string name, std::string workspace){
+    name_=name;
+    Id_=id;
+    workspace_=workspace;
+}
+
+TeacherId Teacher::Id() {
+    return this->Id_;
+}
+std::string Teacher::Name() {
+    return this->name_;
+}
+std::string Teacher::Department(){
+    return this->workspace_;
+}
+
+bool Teacher::operator ==(Teacher a) const {
+    if (this->Id_ == a.Id() && this->workspace_ == a.Department() && this->name_ == a.Name()){ return true; }
+    else { return false; }
+}
+
+bool Teacher::operator !=(Teacher a) const {
+    if (this->Id_ != a.Id() || this->workspace_ != a.Department() || this->name_ != a.Name()){ return true; }
+    else { return false; }
+}
---
+++ lab12/teacherhash/TeacherHash.h
@@ -0,0 +1,42 @@
+//
+// Created by Grzegorz on 11.06.2017.
+//
+
+#ifndef JIMP_EXERCISES_TEACHERHASH_H
+#define JIMP_EXERCISES_TEACHERHASH_H
+
+#include <string>
+
+namespace academia{
+    class TeacherId{
+    public:
+        TeacherId();
+        TeacherId(int value);
+        operator int() const;
+
+
+    private:
+        int value_;
+    };
+
+    class Teacher{
+    public:
+        Teacher(TeacherId id, std::string name, std::string workspace);
+        TeacherId Id();
+        std::string Name();
+        std::string Department();
+        bool operator ==(Teacher a) const;
+        bool operator !=(Teacher a) const;
+    private:
+        TeacherId Id_;
+        std::string name_;
+        std::string workspace_;
+    };
+
+    class TeacherHash{
+    public:
+        int operator ()(const Teacher teacher) const;
+    };
+}
+
+#endif //JIMP_EXERCISES_TEACHERHASH_H
